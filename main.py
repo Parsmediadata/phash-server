5755aa8ead32dc572ae7df922db587bd79275727
Parsmediadata <Parsmediadata@gmail.com>
2025-03-15 22:04:13 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 import imagehash
+import base64
 from flask import Flask, request, jsonify
 from PIL import Image
 import io
@@ -8,19 +9,26 @@ app = Flask(__name__)
 
 @app.route('/phash', methods=['POST'])
 def hash_image():
-    if 'image' not in request.files:
-        return jsonify({"error": "No image uploaded"}), 400
-
     try:
-        file = request.files['image']
-        img = Image.open(io.BytesIO(file.read()))
-        hash_value = str(imagehash.phash(img))  # محاسبه pHash
+        # بررسی اینکه آیا `Base64` در درخواست وجود دارد
+        data = request.json
+        if 'image_base64' not in data:
+            return jsonify({"error": "No Base64 image provided"}), 400
+
+        # دریافت و تبدیل `Base64` به تصویر
+        image_data = base64.b64decode(data['image_base64'])
+        img = Image.open(io.BytesIO(image_data))
+
+        # محاسبه `pHash`
+        hash_value = str(imagehash.phash(img))
+
         return jsonify({"phash": hash_value})
 
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 10000))  # دریافت مقدار PORT از محیط، در غیر این صورت 10000
+    port = int(os.environ.get("PORT", 10000))  # دریافت مقدار PORT از محیط
     app.run(host='0.0.0.0', port=port)
+
 
