5199dc43c0ac64b469522fa02c4c157e8d833286
Parsmediadata <Parsmediadata@gmail.com>
2025-03-15 21:10:36 +0330

    Update main.py

--- main.py
+++ main.py
@@ -5,12 +5,20 @@ import io
 
 app = Flask(__name__)
 
-@app.route('/hash', methods=['POST'])
+@app.route('/phash', methods=['POST'])
 def hash_image():
-    file = request.files['image']
-    img = Image.open(io.BytesIO(file.read()))
-    hash_value = str(imagehash.phash(img))
-    return jsonify({"hash": hash_value})
+    if 'image' not in request.files:
+        return jsonify({"error": "No image uploaded"}), 400
+
+    try:
+        file = request.files['image']
+        img = Image.open(io.BytesIO(file.read()))
+        hash_value = str(imagehash.phash(img))  # محاسبه pHash
+        return jsonify({"phash": hash_value})
+
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=10000)
+
