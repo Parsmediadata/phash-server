a0e104a5d71a1c4d6763806837c8e1a4cf8d6bfc
Parsmediadata <Parsmediadata@gmail.com>
2025-05-13 15:27:28 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,7 @@
-from fastapi import FastAPI, UploadFile, File
-import face_recognition
-import io
-from PIL import Image
+from fastapi import FastAPI
 
 app = FastAPI()
 
 @app.get("/")
-def home():
-    return {"message": "Face encoding API is running"}
-
-@app.post("/encode_face/")
-async def encode_face(file: UploadFile = File(...)):
-    image_bytes = await file.read()
-    image = face_recognition.load_image_file(io.BytesIO(image_bytes))
-    encodings = face_recognition.face_encodings(image)
-
-    if not encodings:
-        return {"error": "No face found"}
-
-    return {"encoding": encodings[0].tolist()}
+def read_root():
+    return {"message": "Hello from Railway!"}
